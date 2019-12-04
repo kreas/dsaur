9c582160e07c1731f3c5f75d5dce3a02dde7e269
Tim Warren <tim.warren@takl.com>
2019-12-04 17:22:31 -0600

    Added packages for testing and password hashing

--- api/mix.exs
+++ api/mix.exs
@@ -42,7 +42,10 @@ defmodule Dsaur.MixProject do
       {:postgrex, ">= 0.0.0"},
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
-      {:plug_cowboy, "~> 2.0"}
+      {:plug_cowboy, "~> 2.0"},
+      {:bcrypt_elixir, "~> 2.0"},
+      {:ex_machina, "~> 2.3", only: :test},
+      {:mix_test_watch, "~> 0.8", only: :dev, runtime: false}
     ]
   end
 
